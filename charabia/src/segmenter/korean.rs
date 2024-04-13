8d50d43f60f472eb2699cd6528143b3f48bea952
JB <wjdqls1342@gmail.com>
2024-04-13 21:48:01 +0900

    fix: Hangul tokenizer changed to spacing-based

--- charabia/src/segmenter/korean.rs
+++ charabia/src/segmenter/korean.rs
@@ -1,7 +1,7 @@
-use lindera_core::mode::{Mode, Penalty};
-use lindera_dictionary::{DictionaryConfig, DictionaryKind};
-use lindera_tokenizer::tokenizer::{Tokenizer, TokenizerConfig};
-use once_cell::sync::Lazy;
+// use lindera_core::mode::{Mode, Penalty};
+// use lindera_dictionary::{DictionaryConfig, DictionaryKind};
+// use lindera_tokenizer::tokenizer::{Tokenizer, TokenizerConfig};
+// use once_cell::sync::Lazy;
 
 use crate::segmenter::Segmenter;
 
@@ -10,19 +10,29 @@ use crate::segmenter::Segmenter;
 /// This Segmenter uses lindera internally to segment the provided text.
 pub struct KoreanSegmenter;
 
-static LINDERA: Lazy<Tokenizer> = Lazy::new(|| {
-    let config = TokenizerConfig {
-        dictionary: DictionaryConfig { kind: Some(DictionaryKind::KoDic), path: None },
-        mode: Mode::Decompose(Penalty::default()),
-        ..TokenizerConfig::default()
-    };
-    Tokenizer::from_config(config).unwrap()
-});
+// static LINDERA: Lazy<Tokenizer> = Lazy::new(|| {
+//     let config = TokenizerConfig {
+//         dictionary: DictionaryConfig { kind: Some(DictionaryKind::KoDic), path: None },
+//         mode: Mode::Decompose(Penalty::default()),
+//         ..TokenizerConfig::default()
+//     };
+//     Tokenizer::from_config(config).unwrap()
+// });
+
+// impl Segmenter for KoreanSegmenter {
+//     fn segment_str<'o>(&self, to_segment: &'o str) -> Box<dyn Iterator<Item = &'o str> + 'o> {
+//         let segment_iterator = LINDERA.tokenize(to_segment).unwrap();
+//         Box::new(segment_iterator.into_iter().map(|token| token.text))
+//     }
+// }
 
 impl Segmenter for KoreanSegmenter {
     fn segment_str<'o>(&self, to_segment: &'o str) -> Box<dyn Iterator<Item = &'o str> + 'o> {
-        let segment_iterator = LINDERA.tokenize(to_segment).unwrap();
-        Box::new(segment_iterator.into_iter().map(|token| token.text))
+        // Create the iterator that will segment the provided text.
+        let segment_iterator = to_segment.split_inclusive(' ');
+
+        // Return the created iterator wrapping it in a Box.
+        Box::new(segment_iterator.into_iter().map(|token| token))
     }
 }
 
@@ -30,13 +40,13 @@ impl Segmenter for KoreanSegmenter {
 mod test {
     use crate::segmenter::test::test_segmenter;
 
-    const TEXT: &str = "한국어의형태해석을실시할수있습니다.";
+    const TEXT: &str = "한국어의형태해석을 실시할수있습니다.";
 
     const SEGMENTED: &[&str] =
-        &["한국어", "의", "형태", "해석", "을", "실시", "할", "수", "있", "습니다", "."];
+        &["한국어의형태해석을", " ", "실시할수있습니다", "."];
 
     const TOKENIZED: &[&str] =
-        &["한국어", "의", "형태", "해석", "을", "실시", "할", "수", "있", "습니다", "."];
+        &["한국어의형태해석을", " ", "실시할수있습니다", "."];
 
     // Macro that run several tests on the Segmenter.
     test_segmenter!(KoreanSegmenter, TEXT, SEGMENTED, TOKENIZED, Script::Hangul, Language::Kor);
